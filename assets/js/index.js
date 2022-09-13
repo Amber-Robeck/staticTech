cfedda8637e6521f032356564709de9a56922868
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-09-12 22:07:38 -0500

    Added carousel array of words to rotate through

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,29 @@
 console.log("connected");
 const bubble = document.querySelector('.bubble');
-const bubbleTextMain = document.querySelector('.bubble :nth-child(2)');
-const bubbleTextSecondary = document.querySelector('.bubble :nth-child(3)');
+let bubbleTextMain = document.querySelector('.bubble :nth-child(2)');
+let bubbleTextSecondary = document.querySelector('.bubble :nth-child(3)');
 console.log(bubbleTextMain.textContent);
+let index = 0;
 
+carouselArray = [
+    'original',
+    'authentic',
+    'legendary',
+    'alluring',
+    'genuine',
+    'beautiful',
+]
+//change to onload
 bubble.addEventListener('mouseover', function () {
     console.log("mouseover");
-})
+    const carousel = setTimeout(rotateCarousel, 5000);
+
+
+});
+
+const rotateCarousel = function () {
+    bubbleTextMain.textContent = `Creating ${carouselArray[index]} websites`
+    // bubble.append(bubbleTextMain);
+    index++;
+
+};
