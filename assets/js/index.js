d67c2c7a426c95bb71e0fb23e3726da1fe0bf84e
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-09-12 21:38:39 -0500

    Added mouseover event listener to bubble, currently logging mouseover

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,9 @@
 console.log("connected");
+const bubble = document.querySelector('.bubble');
+const bubbleTextMain = document.querySelector('.bubble :nth-child(2)');
+const bubbleTextSecondary = document.querySelector('.bubble :nth-child(3)');
+console.log(bubbleTextMain.textContent);
+
+bubble.addEventListener('mouseover', function () {
+    console.log("mouseover");
+})
